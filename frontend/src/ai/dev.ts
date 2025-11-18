ea09b89696b481a169b4d6ba21a1f5fc6dc8229b
figure-2 <pencilfoxs@gmail.com>
2025-11-18 15:30:52 +0900

    Code_추가

--- frontend/src/ai/dev.ts
+++
@@ -1,8 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/royal-answer-based-on-location.ts';
-import '@/ai/flows/receive-smart-nearby-recommendations.ts';
-import '@/ai/flows/generate-visual-explanations.ts';
-import '@/ai/flows/speech-to-text.ts';
-import '@/ai/flows/text-to-speech.ts';
